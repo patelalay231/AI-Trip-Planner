ba91262ee0289eee1b793206bde710ec9fa28b45
patelalay231 <patelalay231@gmail.com>
2025-02-28 22:15:41 +0530

    fixed prompt

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -19,7 +19,7 @@ app.get("/", (req: Request, res: Response) => {
 // Routes
 app.post("/api/trip/generate-trip", async (req: Request, res: Response) => {
     const body = req.body;
-    const prompt = `Generating trip for ${body.place.value.description} for ${body.days} days with a budget of ${body.budgetData.title} for ${body.travelersData.title}.Give me hotel options list with hotelName, hotel address, gro cordinates ,price with currency sign, hotel image url, rating, description, abd suggest iliternary with placename, placedatils, place image url, gro cordinates, ticket pricing, rating, time travel each of the location for 3 days with each day plan with best time to visist in json format.\n`;
+    const prompt = `Generating trip for ${body.place.value.description} for ${body.days} days with a budget of ${body.budgetData.title} for ${body.travelersData.title}.Give me hotel options list with hotelName, hotel address, gro cordinates ,price with currency sign, hotel image url, rating, description, abd suggest iliternary with placename, placedatils, place image url, gro cordinates, ticket pricing, rating, time travel each of the location for ${body.days} days with each day plan with best time to visist in json format.\n`;
     const response = await chatSession.sendMessage(prompt);
     const result = response.response.candidates[0].content.parts[0].text;
     res.json({ result });
